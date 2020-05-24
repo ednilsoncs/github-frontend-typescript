0b5bcc5e83af17761180b89dc4bf783f9e828797
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-05-24 20:39:02 -0300

    feat: integration between redux and repository Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import RepositoryList from './Components/RepositoryList';
+import store from './store';
 
-function App() {
-  return <div className="App" />;
-}
+const App: React.FC = () => (
+  <Provider store={store}>
+    <RepositoryList />
+  </Provider>
+);
 
 export default App;
---
+++ src/Components/RepositoryList/index.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { Repository } from '../../store/ducks/repositories/types';
+import { ApplicationState } from '../../store';
+import * as RepositoriesActions from '../../store/ducks/repositories/actions';
+
+interface StateProps {
+  repositories: Repository[];
+}
+
+interface DispatchProps {
+  loadRequest(): void;
+}
+
+type Props = StateProps & DispatchProps;
+
+const RepositoryList: React.FC<Props> = (props) => {
+  const { repositories, loadRequest } = props;
+  useEffect(() => {
+    loadRequest();
+  }, [loadRequest]);
+  return (
+    <ul>
+      {repositories.map((repository) => (
+        <li>{repository.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({
+  repositories: state.repositories.data,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(RepositoriesActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(RepositoryList);

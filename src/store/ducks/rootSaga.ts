00a4bb05b05b88476e39663199fd638994a6a448
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-05-24 21:29:47 -0300

    feat: added integration with redux-saga

---
+++ src/store/ducks/rootSaga.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { all, takeLatest } from 'redux-saga/effects';
+import { RepositoriesTypes } from './repositories/types';
+import { load } from './repositories/sagas';
+
+export default function* rootSaga() {
+  return yield all([takeLatest(RepositoriesTypes.LOAD_REQUEST, load)]);
+}
